be30257857d3f838574b1db649d7125307b5ad48
Shannon Quinn <srquinn1@gmail.com>
2018-10-05 15:35:02 -0400

    implemented Player

--- src/main/java/com/webcheckers/application/PlayerLobby.java
+++ src/main/java/com/webcheckers/application/PlayerLobby.java
@@ -1,4 +1,45 @@
 package com.webcheckers.application;
 
+import com.webcheckers.model.Player;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ *
+ * @author Shannon Quinn
+ */
 public class PlayerLobby {
+
+    Set<Player> players;
+
+    public PlayerLobby(){
+        players = new HashSet<>();
+    }
+
+    public boolean addPlayer(String name){
+
+        return false;
+    }
+
+    public boolean isValid(String name){
+
+        if(name.matches("[a-zA-Z_0-9]+$")){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PlayerLobby pl = new PlayerLobby();
+        boolean one = pl.isValid("this name");
+        boolean two = pl.isValid("123name");
+        boolean three = pl.isValid("");
+
+        System.out.println("this name " + one);
+        System.out.println("123name " + two);
+        System.out.println("" + three);
+
+    }
 }
--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,4 +1,29 @@
 package com.webcheckers.model;
 
+/**
+ * class that represents a player
+ * @author Shannon Quinn
+ */
 public class Player {
+
+    /**
+     * user name of this player
+     */
+    private String playerName;
+
+    /**
+     * creates a player
+     * @param playerName name of player
+     */
+    public Player(String playerName){
+        this.playerName = playerName;
+    }
+
+    /**
+     * getter for playerName
+     * @return playerName
+     */
+    public String getPlayerName() {
+        return playerName;
+    }
 }
