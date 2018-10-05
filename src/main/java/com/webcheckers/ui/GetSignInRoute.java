04de6db39f270249bfe4b2ea60c683308c44119c
Shannon Quinn <srquinn1@gmail.com>
2018-10-05 15:55:57 -0400

    added back GetSignInRoute

--- src/main/java/com/webcheckers/ui/GetSignInRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -1,4 +1,19 @@
 package com.webcheckers.ui;
 
-public class GetSignInRoute {
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class GetSignInRoute implements Route {
+
+    private Renderer renderer;
+
+    public GetSignInRoute( Renderer renderer ) {
+        this.renderer = renderer;
+    }
+
+    @Override
+    public Object handle( Request request, Response response ) throws Exception {
+        return renderer.render();
+    }
 }
