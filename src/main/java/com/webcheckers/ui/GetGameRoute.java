08bb03e75825268e9d03284d93e44ee9700ecf34
sflemingo <spencer@fleming.net>
2018-10-09 14:44:38 -0400

    commented the things I need to implement in method

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -24,6 +24,11 @@ public class GetGameRoute implements Route {
     @Override
     public Object handle(Request request, Response response) throws Exception {
         //need to implement GamePageRenderer before I can write this
+
+        //if you select player not in game: start game with you as starting (red)player
+        //if you select a player already in a game: return to homepage with an error
+        //if you are selected and you are not in a game: start a game with you as white player
+
         return renderer.render(request.session());
     }
 }
