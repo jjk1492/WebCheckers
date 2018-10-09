978fd65f7a82a3dbef487c66cd3fa24f6d9e408d
John Knecht V <jjk1492@rit.edu>
2018-10-09 18:15:13 -0400

    Fixed an issue with interface implementation

--- src/main/java/com/webcheckers/ui/GameRenderer.java
+++ src/main/java/com/webcheckers/ui/GameRenderer.java
@@ -1,5 +1,16 @@
 package main.java.com.webcheckers.ui;
 
-public class GameRenderer implements Renderer {
+import java.util.Map;
 
+public class GameRenderer implements com.webcheckers.ui.Renderer {
+
+    @Override
+    public Object render(Session session) {
+        return null;
+    }
+
+    @Override
+    public Object render(Session session, Map<String, Object> model) {
+        return null;
+    }
 }
