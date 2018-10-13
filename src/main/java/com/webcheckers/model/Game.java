37853eaa8bd73f1f826dfb050fb8fba4b4d60500
John Knecht V <jjk1492@rit.edu>
2018-10-13 10:50:06 -0400

    Completed game renderer and stubbed out some necessary statements in the game class

--- src/main/java/com/webcheckers/model/Game.java
+++ src/main/java/com/webcheckers/model/Game.java
@@ -1,4 +1,45 @@
-package main.java.com.webcheckers.model;
+package com.webcheckers.model;
+
+
+import java.util.Objects;
 
 public class Game {
+
+    private Player redPlayer;
+    private Player whitePlayer;
+    private Player currentPlayer;
+
+
+    public Game(Player redPlayer, Player whitePlayer) {
+        this.redPlayer = redPlayer;
+        this.whitePlayer = whitePlayer;
+        this.currentPlayer = redPlayer;
+    }
+
+    public Player getRedPlayer() {
+        return redPlayer;
+    }
+
+    public Player getWhitePlayer() {
+        return whitePlayer;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(redPlayer, game.redPlayer) &&
+                Objects.equals(whitePlayer, game.whitePlayer) &&
+                Objects.equals(currentPlayer, game.currentPlayer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(redPlayer, whitePlayer, currentPlayer);
+    }
 }
