8d55ae9d4168b41e216d8da32ba957be5fff141e
sflemingo <spencer@fleming.net>
2018-10-13 14:15:20 -0400

    adding application tier for game

---
+++ src/main/java/com/webcheckers/application/GameCenter.java
@@ -0,0 +1,75 @@
+package com.webcheckers.application;
+
+import com.webcheckers.model.Game;
+import com.webcheckers.model.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * application tier for game creator
+ *
+ * @author Spencer Fleming
+ */
+public class GameCenter {
+    private static GameCenter INSTANCE = null;
+    private static PlayerLobby playerLobby;
+
+    public static GameCenter getInstance() {
+        if ( INSTANCE == null ) {
+            INSTANCE = new GameCenter();
+        }
+        return INSTANCE;
+    }
+
+    private Map<String,Game> playersInGame;
+
+    private GameCenter() {
+        playersInGame = new HashMap<>();
+    }
+
+    /**
+     * makes a game
+     * adds the game to the map at the redPlayer name and whitePlayer name
+     * @param redPlayer name of the red player
+     * @param whitePlayer name of the white player
+     * @return boolean
+     */
+    public synchronized boolean addGame( String redPlayer, String whitePlayer ) {
+
+        playerLobby = PlayerLobby.getInstance();
+        Player red = playerLobby.getPlayer(redPlayer);
+        Player white = playerLobby.getPlayer(whitePlayer);
+
+        if ( red == null || white == null ) {
+            return false;
+        }
+        if ( playersInGame.containsKey( redPlayer) || playersInGame.containsKey( whitePlayer )) {
+            return false;
+        }
+
+        Game game = new Game(red, white);
+        playersInGame.put(redPlayer, game);
+        playersInGame.put(whitePlayer, game);
+
+        return true;
+    }
+
+    /**
+     * returns a game currently playing
+     * @param playerName one of the players in the game
+     */
+    public synchronized Game getGame(String playerName){
+        return playersInGame.get(playerName);
+    }
+
+    /**
+     * removes players from playersInGame
+     * @param player1 player we want to remove
+     * @param player2 player we want to remove
+     */
+    public synchronized void finishedGame( String player1, String player2 ) {
+        playersInGame.remove(player1);
+        playersInGame.remove(player2);
+    }
+}
