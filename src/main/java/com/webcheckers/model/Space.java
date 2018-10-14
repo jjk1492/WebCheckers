44f3a1be5c461d53e27693876fd3750f281097a7
John Knecht V <jjk1492@rit.edu>
2018-10-14 19:44:18 -0400

    finished the space class

--- src/main/java/com/webcheckers/model/Space.java
+++ src/main/java/com/webcheckers/model/Space.java
@@ -4,10 +4,23 @@ public class Space {
 
     private int cellIdx;
     private boolean isValid;
+    private Piece piece;
 
     public Space(int cellIdx){
         this.cellIdx = cellIdx;
         this.isValid = false;
+        this.piece = null;
     }
 
+    public int getCellIdx() {
+        return cellIdx;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
 }
