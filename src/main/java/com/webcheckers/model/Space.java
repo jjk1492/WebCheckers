337918282a9bf5180e3a108aa23ea195e5f9148f
sflemingo <spencer@fleming.net>
2018-10-14 19:34:10 -0400

    adding stuff to board

---
+++ src/main/java/com/webcheckers/model/Space.java
@@ -0,0 +1,13 @@
+package com.webcheckers.model;
+
+public class Space {
+
+    private int cellIdx;
+    private boolean isValid;
+
+    public Space(int cellIdx){
+        this.cellIdx = cellIdx;
+        this.isValid = false;
+    }
+
+}
