bfadd6f61cd3bf4f329ea2f1cd33e9cec38717ee
John Knecht V <jjk1492@rit.edu>
2018-10-14 20:25:06 -0400

    GameRenderer now keeps players at the homepage if they click on a player that is already in game

--- src/main/java/com/webcheckers/ui/GameRenderer.java
+++ src/main/java/com/webcheckers/ui/GameRenderer.java
@@ -1,6 +1,7 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.application.GameCenter;
+import com.webcheckers.application.PlayerLobby;
 import com.webcheckers.model.Game;
 import com.webcheckers.model.Player;
 import spark.ModelAndView;
@@ -18,9 +19,17 @@ public class GameRenderer implements com.webcheckers.ui.Renderer {
 
     private ActiveColor activeColor = ActiveColor.RED;
 
+    // Home view constants
+    private static final String VIEW_NAME_HOME = "home.ftl";
+    private static final String NO_NAME_STRING = "";
+    private static final String SIGNED_IN_ATTR = "signedIn";
+    private static final String PLAYER_LIST_ATTR = "players";
+    private static final String TITLE_ATTR = "title";
+    private static final String DEFAULT_TITLE = "Welcome!";
 
+
+    // Game View Constant
     private static final String VIEW_NAME = "game.ftl";
-
     private static final String CURRENT_PLAYER_ATTR = "currentPlayer";
     private static final String VIEW_MODE_ATTR = "viewMode";
     private static final String RED_PLAYER_ATTR = "redPlayer";
@@ -40,13 +49,33 @@ public class GameRenderer implements com.webcheckers.ui.Renderer {
         return render( session, new HashMap<>() );
     }
 
+
+
     @Override
     public Object render(Session session, Map<String, Object> model) {
+
+
 
         String name = session.attribute(PLAYER_NAME_ATTR);
         GameCenter gameCenter = GameCenter.getInstance();
 
         Game currentGame = gameCenter.getGame(name);
+
+        if(currentGame == null)
+        {
+            PlayerLobby lobby = PlayerLobby.getInstance();
+
+            boolean signedIn = name != null;
+            if ( !signedIn ) {
+                name = NO_NAME_STRING;
+            }
+
+            model.put( SIGNED_IN_ATTR, signedIn );
+            model.put( PLAYER_NAME_ATTR, name );
+            model.put( PLAYER_LIST_ATTR, lobby.getAllPlayers() );
+            model.put( TITLE_ATTR, DEFAULT_TITLE );
+            return templateEngine.render(new ModelAndView(model , VIEW_NAME_HOME));
+        }
 
         //send to game
         Player redPlayer = currentGame.getRedPlayer();
