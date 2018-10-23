77e44c563bfc14881a8ebc65b5ed48526bfc9dc0
Shannon Quinn <srquinn1@gmail.com>
2018-10-23 10:46:05 -0400

    tests for player count and adding new users

--- src/test/java/com/webcheckers/application/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/application/PlayerLobbyTest.java
@@ -66,4 +66,49 @@ class PlayerLobbyTest {
         assertTrue( name5, combo + message );
 
     }
+
+    /**
+     * test to make sure a new player can not use a name already in use
+     */
+    @Test
+    void addDuplicateUsers(){
+        String name = "abc";
+
+        boolean firstAdd = pl.addPlayer( name );
+        assertTrue( firstAdd, name + " should have been added here." );
+
+        boolean secondAdd = pl.addPlayer( name );
+        assertFalse( secondAdd, name + " should not have been added here." );
+    }
+
+    /**
+     * tests that users with different names can be added
+     */
+    @Test
+    void addUsers(){
+        String message = "should have been added";
+        String name1  = "The Kraken";
+        String name2 = "Buttercup22";
+
+        boolean add = pl.addPlayer( name1 );
+        assertTrue( add, name1 + message );
+
+        boolean add2 = pl.addPlayer( name2 );
+        assertTrue( add2, name2 + message );
+    }
+
+    /**
+     * tests if getPlayerCount is correct
+     */
+    @Test
+    void PlayerCount(){
+        Integer actual = pl.getPlayerCount();
+        Integer expected = 3;
+        assertEquals( expected, actual, "Size should be 3" );
+    }
+
+    @Test
+    void getPlayers(){
+
+    }
 }
