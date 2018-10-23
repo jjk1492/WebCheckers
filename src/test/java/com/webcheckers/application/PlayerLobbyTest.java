03209a0b9d0f68bc57c2ac6388593f3fb5265f0d
Shannon Quinn <srquinn1@gmail.com>
2018-10-23 10:29:45 -0400

    added spaces for Zeke

--- src/test/java/com/webcheckers/application/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/application/PlayerLobbyTest.java
@@ -8,9 +8,12 @@ class PlayerLobbyTest {
 
     private PlayerLobby pl = PlayerLobby.getInstance();
 
+    /**
+     * test to make sure pl is not null
+     */
     @Test
     void instanceNotNull() {
-        assertNotNull(pl,"Instance is null");
+        assertNotNull( pl,"Instance is null" );
     }
 
     /**
@@ -23,14 +26,14 @@ class PlayerLobbyTest {
         String invalidChars = "!*inVaLId*";
         String spaceStart = " inValid";
 
-        boolean name1 = pl.isValid(empty);
+        boolean name1 = pl.isValid( empty );
         assertFalse( name1,"Empty string should not be a valid username" );
 
-        boolean name2 = pl.isValid(invalidChars);
-        assertFalse(name2, "Names with non-alphanumeric characters should not be a valid username");
+        boolean name2 = pl.isValid( invalidChars );
+        assertFalse( name2, "Names with non-alphanumeric characters should not be a valid username" );
 
-        boolean name3 = pl.isValid(spaceStart);
-        assertFalse(name3, "Names that with a space should not be a valid username");
+        boolean name3 = pl.isValid( spaceStart );
+        assertFalse( name3, "Names that with a space should not be a valid username" );
 
     }
 
@@ -40,27 +43,27 @@ class PlayerLobbyTest {
      */
     @Test
     void validName(){
-        String message = " should be a valid name";
+        String message = "should be a valid name";
         String one_alpha = "a";
         String one_num = "1";
         String space = "The Kraken";
         String underscore = "the_kraken";
         String combo = "Cats637 38_";
 
-        boolean name1 = pl.isValid(one_alpha);
+        boolean name1 = pl.isValid( one_alpha );
         assertTrue( name1, one_alpha + message );
 
-        boolean name2 = pl.isValid(one_num);
-        assertTrue(name2, one_num + message);
+        boolean name2 = pl.isValid( one_num );
+        assertTrue( name2, one_num + message );
 
-        boolean name3 = pl.isValid(space);
-        assertTrue(name3, space + message);
+        boolean name3 = pl.isValid( space );
+        assertTrue( name3, space + message );
 
-        boolean name4 = pl.isValid(underscore);
-        assertTrue(name4, underscore + message);
+        boolean name4 = pl.isValid( underscore );
+        assertTrue( name4, underscore + message );
 
-        boolean name5 = pl.isValid(combo);
-        assertTrue(name5, combo + message);
+        boolean name5 = pl.isValid( combo );
+        assertTrue( name5, combo + message );
 
     }
 }
