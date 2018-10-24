7aa7c6e925720f1049b70a4fe7766e0434faa18b
Zeke <zeke@zekemiller.me>
2018-10-24 11:03:42 -0400

    HomePageRenderer testing null player lobby

--- src/test/java/com/webcheckers/ui/HomePageRendererTest.java
+++ src/test/java/com/webcheckers/ui/HomePageRendererTest.java
@@ -3,8 +3,10 @@ package com.webcheckers.ui;
 import com.webcheckers.application.PlayerLobby;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import spark.TemplateEngine;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Zeke Miller
@@ -15,10 +17,22 @@ public class HomePageRendererTest {
     public void testNullTemplateEngine() {
         Executable testBothNull = () -> new HomePageRenderer( null, null );
         assertThrows( NullPointerException.class, testBothNull,
-                      "expected an exception from null engine" );
-        Executable testEngineNull =
-                () -> new HomePageRenderer( null, new PlayerLobby() );
+                      "expected an exception from null arguments" );
+        PlayerLobby lobby = new PlayerLobby();
+        Executable testEngineNull = () -> new HomePageRenderer( null, lobby );
         assertThrows( NullPointerException.class, testEngineNull,
                       "expected an exception from null engine" );
+    }
+
+    @Test
+    public void testNullPlayerLobby() {
+        Executable testBothNull = () -> new HomePageRenderer( null, null );
+        assertThrows( NullPointerException.class, testBothNull,
+                      "expected an exception from null arguments" );
+
+        TemplateEngine engine = mock(TemplateEngine.class);
+        Executable testLobbyNull = () -> new HomePageRenderer( engine, null );
+        assertThrows( NullPointerException.class, testLobbyNull,
+                      "expected an exception from null lobby" );
     }
 }
