2df922b1e2e537ad5577a12f0a0673723c840ee6
Zeke <zeke@zekemiller.me>
2018-10-31 19:09:17 -0400

    Documentation in PostCheckTurnRoute

--- src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
@@ -9,15 +9,31 @@ import spark.Route;
 
 import static com.webcheckers.ui.PostSignInRoute.PLAYER_NAME_ATTR;
 
+/**
+ * Handles POST /checkTurn to see if it is the player's turn
+ *
+ * @author Zeke Miller
+ */
 public class PostCheckTurnRoute implements Route {
 
     private GameCenter gameCenter;
 
+    /**
+     * Constructor with a gameCenter for tracking games
+     * @param gameCenter the gameCenter to use for logic
+     */
     public PostCheckTurnRoute( GameCenter gameCenter ) {
         this.gameCenter = gameCenter;
     }
 
 
+    /**
+     * Handles the HTTP request
+     * @param request the Spark request object
+     * @param response the Spark response object
+     * @return JSonized InfoMessage object with if it's the player's turn or not
+     * @throws Exception shouldn't throw any exceptions
+     */
     @Override
     public Object handle(Request request, Response response) throws Exception {
         String name = request.session().attribute( PLAYER_NAME_ATTR );
