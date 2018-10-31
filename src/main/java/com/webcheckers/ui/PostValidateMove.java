b3915aff53ccf2f9fcf49d928c56ab704f09d37f
John Knecht V <jjk1492@rit.edu>
2018-10-31 13:07:33 -0400

    Created the PostValidateMove toute

---
+++ src/main/java/com/webcheckers/ui/PostValidateMove.java
@@ -0,0 +1,12 @@
+package com.webcheckers.ui;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PostValidateMove implements Route {
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return null;
+    }
+}
