b03a4c7df0b064d090cf0516dce0591c127581e8
Zeke <zeke@zekemiller.me>
2018-11-01 17:39:51 -0400

    Created some happy path tests for PostCheckTurnRoute

---
+++ src/test/java/com/webcheckers/ui/PostCheckTurnRouteTest.java
@@ -0,0 +1,134 @@
+package com.webcheckers.ui;
+
+import com.google.gson.Gson;
+import com.webcheckers.application.GameCenter;
+import com.webcheckers.model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+
+import static com.webcheckers.ui.PostSignInRoute.PLAYER_NAME_ATTR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Zeke Miller
+ */
+public class PostCheckTurnRouteTest {
+
+    private static final String redPlayerName = "red";
+    private static final String whitePlayerName = "white";
+    private static final Player redPlayer = mock( Player.class );
+    private static final Player whitePlayer = mock( Player.class );
+    private static final Game gameMock = mock( Game.class );
+    private static final Response responseMock = mock( Response.class );
+    private static final GameCenter centerMock = mock( GameCenter.class );
+    private static final Session sessionMock = mock( Session.class );
+    private static final Request requestMock = mock( Request.class );
+    private static final Gson gson = new Gson();
+    private static final Message trueMessage = new InfoMessage( "true" );
+    private static final String trueJson = gson.toJson( trueMessage );
+    private static final Message falseMessage = new InfoMessage( "false" );
+    private static final String falseJson = gson.toJson( falseMessage );
+
+    private PostCheckTurnRoute route;
+
+    @BeforeEach
+    public void setup() {
+
+        // getName same for all
+        when( redPlayer.getName() ).thenReturn( redPlayerName );
+        when( whitePlayer.getName() ).thenReturn( whitePlayerName );
+
+        // getRedPlayer and getWhitePlayer same for all
+        when( gameMock.getRedPlayer() ).thenReturn( redPlayer );
+        when( gameMock.getWhitePlayer() ).thenReturn( whitePlayer );
+
+        // shouldn't be accessing board
+        when( gameMock.getRedBoard() ).thenReturn( null );
+        when( gameMock.getWhiteBoard() ).thenReturn( null );
+
+        // isPlayerInGame same for all
+        when( centerMock.isPlayerInGame( redPlayerName ) ).thenReturn( true );
+        when( centerMock.isPlayerInGame( whitePlayerName ) ).thenReturn( true );
+
+        // getGame same for all
+        when( centerMock.getGame( redPlayerName ) ).thenReturn( gameMock );
+        when( centerMock.getGame( whitePlayerName ) ).thenReturn( gameMock );
+
+        // getOpponent same for all
+        when( centerMock.getOpponent( redPlayerName ) ).thenReturn( whitePlayerName );
+        when( centerMock.getOpponent( whitePlayerName ) ).thenReturn( redPlayerName );
+
+        when( requestMock.session() ).thenReturn( sessionMock );
+
+        route = new PostCheckTurnRoute( centerMock );
+    }
+
+    private void setupPlayers( Color active, String current ) {
+        when( centerMock.isPlayerActive( redPlayerName ) ).thenReturn( active.equals( Color.RED ) );
+        when( centerMock.isPlayerActive( whitePlayerName ) ).thenReturn( active.equals( Color.WHITE ) );
+
+        when( sessionMock.attribute( PLAYER_NAME_ATTR ) ).thenReturn( current );
+    }
+
+    private String getJson() {
+        String result = null;
+
+        try {
+            result = (String)route.handle( requestMock, responseMock );
+        }
+        catch ( Exception e ) {
+            fail();
+        }
+        return result;
+    }
+
+    @Test
+    public void testWhitePlayerWhiteTurn() {
+
+        setupPlayers( Color.WHITE, whitePlayerName );
+
+        String expected = trueJson;
+        String actual = getJson();
+
+        assertEquals( expected, actual, "Expected a true info message" );
+    }
+
+    @Test
+    public void testWhitePlayerRedTurn() {
+
+        setupPlayers( Color.RED, whitePlayerName );
+
+        String expected = falseJson;
+        String actual = getJson();
+
+        assertEquals( expected, actual, "Expected a false info message" );
+    }
+
+    @Test
+    public void testRedPlayerWhiteTurn() {
+
+        setupPlayers( Color.WHITE, redPlayerName );
+
+        String expected = falseJson;
+        String actual = getJson();
+
+        assertEquals( expected, actual, "Expected a false info message" );
+    }
+
+    @Test
+    public void testRedPlayerRedTurn() {
+
+        setupPlayers( Color.RED, redPlayerName );
+
+        String expected = trueJson;
+        String actual = getJson();
+
+        assertEquals( expected, actual, "Expected a true info message" );
+    }
+}
