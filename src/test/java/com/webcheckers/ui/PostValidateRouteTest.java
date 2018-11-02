5833ef805e5a569b5cd60796630617983f0ada42
John Knecht V <jjk1492@rit.edu>
2018-11-01 22:31:55 -0400

    Created the PostValidateMoveTest Class

---
+++ src/test/java/com/webcheckers/ui/PostValidateRouteTest.java
@@ -0,0 +1,16 @@
+package com.webcheckers.ui;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import com.webcheckers.application.GameCenter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import spark.*;
+
+@Tag("UI-Tier")
+public class PostValidateRouteTest {
+}
