8479b4065e083356fb85c92c7f68d0d87852ee51
nick-sander <33533737+nws2007@users.noreply.github.com>
2018-11-05 13:21:04 -0500

    added testing for Piece in Space class

--- src/test/java/com/webcheckers/model/SpaceTest.java
+++ src/test/java/com/webcheckers/model/SpaceTest.java
@@ -11,18 +11,28 @@ import static org.junit.jupiter.api.Assertions.*;
 @Tag("Model-Tier")
 public class SpaceTest {
 
+    private Space space;
+
     @Test
     public void testNull() {
-        Space space = new Space( 3 );
+        space = new Space( 7 );
         assertNotNull( space, "expected non-null object" );
         assertNull( space.getPiece(), "expected null message" );
     }
 
     @Test
     public void testValid() {
-        Space space = new Space( 3 );
+        space = new Space( 3 );
         assertFalse(space.isValid(), "expected false (non-valid space).");
         space.setValid(true);
         assertTrue(space.isValid(), "expected true (valid space).");
+    }
+
+    @Test
+    public void testPiece() {
+        space = new Space(0);
+        Piece piece = new Piece(Color.RED, Piece.Type.SINGLE);
+        space.setPiece(piece);
+        assertEquals(piece, space.getPiece());
     }
 }
